5e9cdfa1918cc2587966a354327c7a29474e2903
Cody Averett <codyaverett@gmail.com>
2014-01-16 00:15:45 -0500

    Update x11vnc.sh
    Adding argument option for password

--- linux/x11vnc.sh
+++ linux/x11vnc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/x11vnc -nap -wait 50 -noxdamage -passwd musicthiry -display :0 -forever -o /var/log/x11vnc.log -bg
+/usr/bin/x11vnc -nap -wait 50 -noxdamage -passwd $1 -display :0 -forever -o /var/log/x11vnc.log -bg
